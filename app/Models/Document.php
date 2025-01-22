9a0b24841247b38806baf35cbfb5de34b760f9e3
Riki Ruswandi <riki@pratamatechsolution.co.id>
2025-01-22 14:25:23 +0700

    feat: spatie logs activity setup

--- app/Models/Document.php
+++ app/Models/Document.php
@@ -5,10 +5,23 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class Document extends Model
 {
-    use HasFactory;
+    use HasFactory, LogsActivity;
+
+    protected static $logName = 'document_activity';
+
+    protected static $logAttributes = ['name', 'file_path', 'extension', 'size', 'mime_type'];
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->logOnly(static::$logAttributes)
+            ->useLogName(static::$logName);
+    }
 
     protected $fillable = ['name', 'file_path', 'extension', 'size', 'mime_type'];
 
--- app/Models/Role.php
+++ app/Models/Role.php
@@ -5,10 +5,23 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class Role extends Model
 {
-    use HasFactory;
+    use HasFactory, LogsActivity;
+
+    protected static $logName = 'role_activity';
+
+    protected static $logAttributes = ['name'];
+
+    public function getActivitylogOptions(): LogOptions
+    {
+        return LogOptions::defaults()
+            ->logOnly(static::$logAttributes)
+            ->useLogName(static::$logName);
+    }
 
     public function users(): HasMany
     {
