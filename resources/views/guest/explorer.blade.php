f87d8beaebe7cc81ccdc726d1c5506ff764d1bd8
Riki Ruswandi <riki@pratamatechsolution.co.id>
2025-02-07 11:20:56 +0700

    feat:sidebar control layer ui

--- resources/views/guest/explorer.blade.php
+++ resources/views/guest/explorer.blade.php
@@ -1,15 +1,190 @@
 <x-guest-layout>
     @section('title', $title) <!-- Mengatur judul halaman -->
     @section('description', $description) <!-- Mengatur deskripsi halaman -->
+    @php
+        $modalDataset = [
+            'title' => 'Layer',
+            'footer' => '',
+        ];
+    @endphp
+    @push('css')
+        <style>
+            #openModalDataset {
+                position: relative;
+            }
 
-    <div class="content" id="main-content">
-        <x-map-container mapId="explorerMapId" :height="'100vh'" />
+            .modal-dialog {
+                position: fixed;
+                width: 100%;
+                margin: 0;
+                padding: 10px;
+            }
+        </style>
+    @endpush
+
+    <div class="d-flex">
+
+        @include('guest.partials.sidebar')
+
+        <div class="content" id="main-content">
+            <x-map-container mapId="explorerMapId" :height="'91vh'" />
+        </div>
     </div>
 
+    @section('modal')
+        <!-- Modal -->
+        <x-modal :id="'openModalDataset'" :data="$modalDataset" :showCancelButton="false" :size="'sm'">
+            <x-slot name="body">
+                <div class="p-2 d-flex justify-content-center align-items-center">
+                    <input class="border-0 p-1 rounded" type="text" name="search-dataset" id="search-dataset"
+                        placeholder="cari.."><button class="border-0 p-1 rounded" type="submit"><i
+                            class="bi bi-search text-success ms-1"></i></button>
+                </div>
+                <div class="mt-1 row p-2 overflow-x-auto body-dataset" style="max-height: 370px; scrollbar-width: none;">
+                    <div class="col-12 text-success text-sm">
+                        <div class="accordion" id="accordionExample">
+                            <div class="accordion-item">
+                                <h2 class="accordion-header" id="headingOne">
+                                    <button class="accordion-button collapsed text-success" type="button"
+                                        data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false"
+                                        aria-controls="collapseOne">
+                                        <i class="bi bi-card-image me-2"></i>Dataset #1
+                                    </button>
+                                </h2>
+                                <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne"
+                                    data-bs-parent="#accordionExample">
+                                    <div class="accordion-body">
+                                        <ul class="list-group overflow-y-auto">
+                                            <li class="list-group-item"><a href="" class="d-flex"><i
+                                                        class="bi bi-plus me-2"></i>Layer</a></li>
+                                            <li class="list-group-item"><a href="" class="d-flex"><i
+                                                        class="bi bi-plus me-2"></i>Layer</a></li>
+                                            <li class="list-group-item"><a href="" class="d-flex"><i
+                                                        class="bi bi-plus me-2"></i>Layer</a></li>
+                                            <li class="list-group-item"><a href="" class="d-flex"><i
+                                                        class="bi bi-plus me-2"></i>Layer</a></li>
+                                            <li class="list-group-item"><a href="" class="d-flex"><i
+                                                        class="bi bi-plus me-2"></i>Layer</a></li>
+                                            <li class="list-group-item"><a href="" class="d-flex"><i
+                                                        class="bi bi-plus me-2"></i>Layer</a></li>
+                                            <li class="list-group-item"><a href="" class="d-flex"><i
+                                                        class="bi bi-plus me-2"></i>Layer</a></li>
+                                            <li class="list-group-item"><a href="" class="d-flex"><i
+                                                        class="bi bi-plus me-2"></i>Layer</a></li>
+                                            <li class="list-group-item"><a href="" class="d-flex"><i
+                                                        class="bi bi-plus me-2"></i>Layer</a></li>
+                                            <li class="list-group-item"><a href="" class="d-flex"><i
+                                                        class="bi bi-plus me-2"></i>Layer</a></li>
+                                            <li class="list-group-item"><a href="" class="d-flex"><i
+                                                        class="bi bi-plus me-2"></i>Layer</a></li>
+                                            <li class="list-group-item"><a href="" class="d-flex"><i
+                                                        class="bi bi-plus me-2"></i>Layer</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="accordion-item">
+                                <h2 class="accordion-header" id="headingTwo">
+                                    <button class="accordion-button collapsed text-success" type="button"
+                                        data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false"
+                                        aria-controls="collapseTwo">
+                                        <i class="bi bi-card-image me-2"></i>Dataset #2
+                                    </button>
+                                </h2>
+                                <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo"
+                                    data-bs-parent="#accordionExample">
+                                    <div class="accordion-body">
+                                        <ul class="list-group overflow-y-auto">
+                                            <li class="list-group-item"><a href="" class="d-flex"><i
+                                                        class="bi bi-plus me-2"></i>Layer</a></li>
+                                            <li class="list-group-item"><a href="" class="d-flex"><i
+                                                        class="bi bi-plus me-2"></i>Layer</a></li>
+                                            <li class="list-group-item"><a href="" class="d-flex"><i
+                                                        class="bi bi-plus me-2"></i>Layer</a></li>
+                                            <li class="list-group-item"><a href="" class="d-flex"><i
+                                                        class="bi bi-plus me-2"></i>Layer</a></li>
+                                            <li class="list-group-item"><a href="" class="d-flex"><i
+                                                        class="bi bi-plus me-2"></i>Layer</a></li>
+                                            <li class="list-group-item"><a href="" class="d-flex"><i
+                                                        class="bi bi-plus me-2"></i>Layer</a></li>
+                                            <li class="list-group-item"><a href="" class="d-flex"><i
+                                                        class="bi bi-plus me-2"></i>Layer</a></li>
+                                            <li class="list-group-item"><a href="" class="d-flex"><i
+                                                        class="bi bi-plus me-2"></i>Layer</a></li>
+                                            <li class="list-group-item"><a href="" class="d-flex"><i
+                                                        class="bi bi-plus me-2"></i>Layer</a></li>
+                                            <li class="list-group-item"><a href="" class="d-flex"><i
+                                                        class="bi bi-plus me-2"></i>Layer</a></li>
+                                            <li class="list-group-item"><a href="" class="d-flex"><i
+                                                        class="bi bi-plus me-2"></i>Layer</a></li>
+                                            <li class="list-group-item"><a href="" class="d-flex"><i
+                                                        class="bi bi-plus me-2"></i>Layer</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="accordion-item">
+                                <h2 class="accordion-header" id="headingThree">
+                                    <button class="accordion-button collapsed text-success" type="button"
+                                        data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false"
+                                        aria-controls="collapseThree">
+                                        <i class="bi bi-card-image me-2"></i>Dataset #3
+                                    </button>
+                                </h2>
+                                <div id="collapseThree" class="accordion-collapse collapse"
+                                    aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                                    <div class="accordion-body">
+                                        <ul class="list-group overflow-y-auto">
+                                            <li class="list-group-item"><a href="" class="d-flex"><i
+                                                        class="bi bi-plus me-2"></i>Layer</a></li>
+                                            <li class="list-group-item"><a href="" class="d-flex"><i
+                                                        class="bi bi-plus me-2"></i>Layer</a></li>
+                                            <li class="list-group-item"><a href="" class="d-flex"><i
+                                                        class="bi bi-plus me-2"></i>Layer</a></li>
+                                            <li class="list-group-item"><a href="" class="d-flex"><i
+                                                        class="bi bi-plus me-2"></i>Layer</a></li>
+                                            <li class="list-group-item"><a href="" class="d-flex"><i
+                                                        class="bi bi-plus me-2"></i>Layer</a></li>
+                                            <li class="list-group-item"><a href="" class="d-flex"><i
+                                                        class="bi bi-plus me-2"></i>Layer</a></li>
+                                            <li class="list-group-item"><a href="" class="d-flex"><i
+                                                        class="bi bi-plus me-2"></i>Layer</a></li>
+                                            <li class="list-group-item"><a href="" class="d-flex"><i
+                                                        class="bi bi-plus me-2"></i>Layer</a></li>
+                                            <li class="list-group-item"><a href="" class="d-flex"><i
+                                                        class="bi bi-plus me-2"></i>Layer</a></li>
+                                            <li class="list-group-item"><a href="" class="d-flex"><i
+                                                        class="bi bi-plus me-2"></i>Layer</a></li>
+                                            <li class="list-group-item"><a href="" class="d-flex"><i
+                                                        class="bi bi-plus me-2"></i>Layer</a></li>
+                                            <li class="list-group-item"><a href="" class="d-flex"><i
+                                                        class="bi bi-plus me-2"></i>Layer</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </x-slot>
+        </x-modal>
+
+    @endsection
     @push('scripts')
+        <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
         <script>
             var $m = jQuery.noConflict();
             $m(document).ready(function() {
+                const sidebar = $m("#sidebar");
+                const content = $m("#main-content");
+                const toggleButtons = $m("#toggleSidebar, #anotherToggle");
+
+                toggleButtons.on("click", function() {
+                    sidebar.toggleClass("closed");
+                    content.toggleClass("shifted");
+                    toggleButtons.toggleClass("closed");
+                });
+
                 initMap('explorerMapId', 'osm', '', {
                     scale: true,
                     fullScreen: true,
@@ -17,6 +192,37 @@
                 }, {
                     dragPan: true,
                     mouseWheelZoom: false
+                });
+
+                // Fungsi untuk menampilkan modal
+                $m(document).on('click', '[data-bs-target="#openModalDataset"]', function() {
+                    $m('.modal-header').css({
+                        cursor: 'move',
+                        color: 'white',
+                        backgroundColor: '#0fac81'
+                    });
+                    $m('.modal-title').attr('style', 'color: white !important');
+                    $m('.modal-content').css({
+                        'background': 'rgba(255, 255, 255, 0.5)',
+                        'backdrop-filter': 'blur(3px)'
+                    });
+
+                    if (!($m('.modal.in').length)) {
+                        $m('.modal-dialog').css({
+                            top: 0,
+                            left: 280
+                        });
+                    }
+                    $m('#openModalDataset').modal({
+                        backdrop: 'static',
+                        keyboard: false,
+                        show: true
+                    });
+                    $m('.modal-backdrop').remove();
+
+                    $m('.modal-dialog').draggable({
+                        handle: ".modal-header"
+                    });
                 });
             });
         </script>
--- resources/views/guest/partials/sidebar.blade.php
+++ resources/views/guest/partials/sidebar.blade.php
@@ -2,17 +2,20 @@
     <style>
         .sidebar-wrapper {
             display: flex;
-            justify-content: space-between;
-            align-items: center;
+            justify-content: flex-start;
+            align-items: start;
             position: relative;
         }
 
         .sidebar {
-            width: 250px;
-            background-color: #343a40;
-            color: white;
-            height: 100vh;
-            position: fixed;
+            width: 260px;
+            background: rgba(255, 255, 255, 0.5);
+            /* Warna dengan transparansi */
+            backdrop-filter: blur(3px);
+            /* Efek blur */
+            height: 91vh;
+            position: absolute;
+            z-index: 999;
             left: 0;
             top: 0;
             transition: transform 0.3s ease;
@@ -23,9 +26,8 @@
         }
 
         .content {
-            margin-left: 250px;
-            padding: 20px;
-            transition: margin-left 0.3s ease;
+            padding: 0;
+            width: 100%;
         }
 
         .content.shifted {
@@ -33,53 +35,87 @@
         }
 
         .toggle-btn {
-            position: fixed;
-            top: 20px;
-            right: 20px;
+            position: absolute;
+            top: 16px;
+            left: 290px;
             z-index: 999;
+            width: 270px;
+            max-width: 270px;
+            transition: transform 0.3s ease;
+            background: rgba(255, 255, 255, 0.5);
+            /* Warna dengan transparansi */
+            backdrop-filter: blur(3px);
+            /* Efek blur */
+        }
+
+        .toggle-btn.closed {
+            transform: translateX(-250px);
+        }
+
+        .b-plus {
+            font-size: 20px;
+            width: 28px;
+            height: 28px;
+            border-radius: 5px;
+            padding: 5px;
+            border: none;
+            background: #007052;
+            color: #ffffff;
+        }
+        .b-arrow-bar-left {
+            font-size: 20px;
+            width: 28px;
+            height: 28px;
+            border-radius: 5px;
+            padding: 5px;
+            border: 2px;
+            border-color: #ffffff;
+        }
+        .aside-head {
+            background: rgba(7, 169, 88, 0.785);
+            padding: 10px;
+            margin: 0;
+            border-radius: 3px;
+        }
+        .aside-content {
+            list-style: none;
+            margin: 0;
+            background: rgba(255, 255, 255, 0.5);
+            padding: 5px;
+            border-radius: 3px;
         }
     </style>
 @endpush
 
 <div class="sidebar-wrapper">
     <!-- Sidebar -->
-    <div class="sidebar" id="sidebar">
+    <div class="sidebar shadow-lg" id="sidebar">
+        <div class="d-flex justify-content-between align-items-center aside-head">
+            <strong class="text-light">Daftar Layer</strong>
+            <div class="d-flex gap-1">
+                <a href="javascript:void(0);" data-bs-toggle="modal" data-bs-target="#openModalDataset" class="b-plus d-flex justify-content-center align-items-center"><i class="bi bi-plus"></i></a>
+
+                <a href="#" id="anotherToggle" class="b-arrow-bar-left d-flex justify-content-center align-items-center link-light"><i class="bi bi-arrow-bar-left"></i></a>
+            </div>
+        </div>
         <div class="p-3">
-            <h5>My Sidebar</h5>
-            <ul class="nav flex-column">
+            <ul class="nav flex-column aside-content mt-3">
                 <li class="nav-item">
-                    <a class="nav-link text-white" href="#"><i class="bi bi-house"></i> Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-white" href="#"><i class="bi bi-person"></i> Profile</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-white" href="#"><i class="bi bi-gear"></i> Settings</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link text-white" href="#"><i class="bi bi-box-arrow-right"></i> Logout</a>
+                    <a class="nav-link text-white" href="#"><i class="bi bi-house"></i> </a>
                 </li>
             </ul>
         </div>
     </div>
 
     <!-- Toggle Sidebar Button -->
-    <button class="btn btn-dark toggle-btn" id="toggleSidebar">
-        <i class="bi bi-list"></i> Toggle Sidebar
-    </button>
+    <div class="toggle-btn rounded shadow-lg border-0 p-2 text-success cursor-pointer d-flex jutify-content-center align-items-center"
+        id="toggleSidebar">
+        <i class="bi bi-list" style="cursor: pointer;"></i>
+        <form action="" method="get">
+            <div class="d-flex jutify-content-center align-items-center">
+                <input type="text" class="ms-2 border-0 p-1" name="search-from-aside" id="search-from-aside" placeholder="cari..">
+                <button class="border-0 p-1" type="submit"><i class="bi bi-search text-success ms-1"></i></button>
+            </div>
+        </form>
+    </div>
 </div>
-@push('scripts')
-    <script>
-        var $m = jQuery.noConflict();
-        $m(document).ready(function() {
-            const sidebar = $m("#sidebar");
-            const content = $m("#main-content");
-            const toggleSidebarBtn = $m("#toggleSidebar");
-
-            toggleSidebarBtn.on("click", function() {
-                sidebar.classList.toggle("closed");
-                content.classList.toggle("shifted");
-            });
-        });
-    </script>
-@endpush
