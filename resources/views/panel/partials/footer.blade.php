dd1602b549ecd76515e49a7f0c7332d5c747c816
Riki Ruswandi <riki@pratamatechsolution.co.id>
2025-03-11 09:18:16 +0700

    fix:footer mobile responsive

--- resources/views/panel/partials/footer.blade.php
+++ resources/views/panel/partials/footer.blade.php
@@ -1,14 +1,20 @@
 <div class="nk-footer">
     <div class="container-fluid">
-        <div class="nk-footer-wrap">
-            <div class="nk-footer-copyright"> <span class="text-primary">&copy;</span> {{ date('Y') }} <a href="https://pratamatechsolution.co.id/" target="_blank">PT.Pratama Solusi Teknologi <em class="icon ni ni-heart-fill text-primary"></em></a> All rights reserved.
+        <div class="nk-footer-wrap d-flex flex-column flex-md-row justify-content-between align-items-center text-center text-md-start">
+            <div class="nk-footer-copyright">
+                <span class="text-primary">&copy;</span> {{ date('Y') }}
+                <a href="https://pratamatechsolution.co.id/" target="_blank">
+                    PT. Pratama Solusi Teknologi <em class="icon ni ni-heart-fill text-primary"></em>
+                </a>
+                All rights reserved.
             </div>
-            <div class="nk-footer-links">
-                <ul class="nav nav-sm">
+            <div class="nk-footer-links mt-2 mt-md-0">
+                <ul class="nav nav-sm justify-content-center justify-content-md-end">
                     <li class="nav-item">
-                        <a class="text-primary"><em class="icon ni ni-globe"></em>
-                            <span
-                                class="ms-1">{{ $app->name }}</span></a>
+                        <a class="text-primary d-flex align-items-center">
+                            <em class="icon ni ni-globe"></em>
+                            <span class="ms-1">{{ $app->name }}</span>
+                        </a>
                     </li>
                 </ul>
             </div>
