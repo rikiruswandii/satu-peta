d568a74f8ca562bb598e68dfcd5b0a75222b9a15
Riki Ruswandi <riki@pratamatechsolution.co.id>
2025-03-11 09:15:12 +0700

    fix:dashboard mobile responsive

--- resources/views/panel/dashboard.blade.php
+++ resources/views/panel/dashboard.blade.php
@@ -2,76 +2,81 @@
     @section('title', $title) <!-- Mengatur judul halaman -->
     @section('description', $description) <!-- Mengatur deskripsi halaman -->
     <div class="nk-block">
-        <div class="row g-3">
+        <div class="row g-3 align-items-stretch">
             <!-- Konten pertama -->
             <div class="col-xxl-3 col-sm-4">
-                <div class="card h-100">
+                <div class="card">
                     <div class="card-body">
                         <h3 class="h5 fw-bold mb-4 text-primary">Selamat Datang!</h3>
-                        <div class="d-flex flex-column gap-2">
-                            <div class="d-flex">
-                                <span class="fw-bold w-25">Nama</span>
-                                <span class="ms-2">:</span>
-                                <span class="ms-2">{{ Auth::user()->name }}</span>
-                            </div>
-                            <div class="d-flex">
-                                <span class="fw-bold w-25">Username</span>
-                                <span class="ms-2">:</span>
-                                <span class="ms-2">{{ Auth::user()->username ? Auth::user()->username : '-' }}</span>
-                            </div>
-                            <div class="d-flex">
-                                <span class="fw-bold w-25">Email</span>
-                                <span class="ms-2">:</span>
-                                <span class="ms-2">{{ Auth::user()->email }}</span>
-                            </div>
-                            <div class="d-flex">
-                                <span class="fw-bold w-25">Terdaftar</span>
-                                <span class="ms-2">:</span>
-                                <span
-                                    class="ms-2">{{ \Carbon\Carbon::parse(Auth::user()->created_at)->isoFormat('D MMMM YYYY, HH:mm:ss') }}</span>
-                            </div>
-                        </div>
+                        <table class="table table-borderless">
+                            <tbody>
+                                <tr>
+                                    <td class="fw-bold w-25">Nama</td>
+                                    <td class="w-1">:</td>
+                                    <td>{{ Auth::user()->name }}</td>
+                                </tr>
+                                <tr>
+                                    <td class="fw-bold w-25">Username</td>
+                                    <td class="w-1">:</td>
+                                    <td>{{ Auth::user()->username ?? '-' }}</td>
+                                </tr>
+                                <tr>
+                                    <td class="fw-bold w-25">Email</td>
+                                    <td class="w-1">:</td>
+                                    <td>{{ Auth::user()->email }}</td>
+                                </tr>
+                                <tr>
+                                    <td class="fw-bold w-25">Terdaftar</td>
+                                    <td class="w-1">:</td>
+                                    <td>{{ \Carbon\Carbon::parse(Auth::user()->created_at)->isoFormat('D MMMM YYYY, HH:mm:ss') }}
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
 
+
             <!-- Konten kedua -->
             <div class="col-xxl-3 col-sm-4">
-                <div class="card h-100">
+                <div class="card">
                     <div class="card-body">
                         <h3 class="h5 fw-bold mb-4 text-primary">Informasi Dasar</h3>
-                        <div class="d-flex flex-column gap-2">
-                            <div class="d-flex">
-                                <span class="fw-bold w-25">Kota</span>
-                                <span class="ms-2">:</span>
-                                <span class="ms-2">{{ ' - ' }}</span>
-                            </div>
-                            <div class="d-flex">
-                                <span class="fw-bold w-25">Provinsi</span>
-                                <span class="ms-2">:</span>
-                                <span class="ms-2">{{ ' - ' }}</span>
-                            </div>
-                            <div class="d-flex">
-                                <span class="fw-bold w-25">Negara</span>
-                                <span class="ms-2">:</span>
-                                <span class="ms-2">{{ ' - ' }}</span>
-                            </div>
-                            <div class="d-flex">
-                                <span class="fw-bold w-25">Alamat IP</span>
-                                <span class="ms-2">:</span>
-                                <span class="ms-2">{{ $data['ip'] }}</span>
-                            </div>
-                            <div class="d-flex">
-                                <span class="fw-bold w-25">OS</span>
-                                <span class="ms-2">:</span>
-                                <span class="ms-2">{{ $data['os'] }}</span>
-                            </div>
-                            <div class="d-flex">
-                                <span class="fw-bold w-25">Peramban</span>
-                                <span class="ms-2">:</span>
-                                <span class="ms-2">{{ $data['browser'] }}</span>
-                            </div>
-                        </div>
+                        <table class="table table-borderless">
+                            <tbody>
+                                <tr>
+                                    <td class="fw-bold w-25">Kota</td>
+                                    <td class="w-1">:</td>
+                                    <td>{{ ' - ' }}</td>
+                                </tr>
+                                <tr>
+                                    <td class="fw-bold w-25">Provinsi</td>
+                                    <td class="w-1">:</td>
+                                    <td>{{ ' - ' }}</td>
+                                </tr>
+                                <tr>
+                                    <td class="fw-bold w-25">Negara</td>
+                                    <td class="w-1">:</td>
+                                    <td>{{ ' - ' }}</td>
+                                </tr>
+                                <tr>
+                                    <td class="fw-bold w-25">Alamat IP</td>
+                                    <td class="w-1">:</td>
+                                    <td>{{ $data['ip'] }}</td>
+                                </tr>
+                                <tr>
+                                    <td class="fw-bold w-25">OS</td>
+                                    <td class="w-1">:</td>
+                                    <td>{{ $data['os'] }}</td>
+                                </tr>
+                                <tr>
+                                    <td class="fw-bold w-25">Peramban</td>
+                                    <td class="w-1">:</td>
+                                    <td>{{ $data['browser'] }}</td>
+                                </tr>
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
@@ -93,7 +98,8 @@
                         <h6 class="card-title text-primary mb-1">{{ Auth::user()->name }}</h6>
                         <p class="fw-semibold text-muted">{{ Auth::user()->role['name'] }}</p>
                     </div>
-                    <a href="{{ route('user.detail', ['id' => Crypt::encrypt(Auth::user()->id)]) }}" class="btn btn-primary w-100 rounded-0 text-white">View Profile</a>
+                    <a href="{{ route('user.detail', ['id' => Crypt::encrypt(Auth::user()->id)]) }}"
+                        class="btn btn-primary w-100 rounded-0 text-white">View Profile</a>
                 </div>
             </div>
         </div>
